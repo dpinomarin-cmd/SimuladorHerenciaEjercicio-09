7fb9e953aea6cdc9ca180208c4508cbbfdf7385e
usuario <usuario@DESKTOP-QOV8DKP>
2026-09-23 11:09:27 -0500

    Creacion del simulador de rol juego

--- src/main/java/com/mycompany/simuladorjuegorol/SimuladorJuegoRol.java
+++ src/main/java/com/mycompany/simuladorjuegorol/SimuladorJuegoRol.java
@@ -3,7 +3,9 @@
  */
 
 package com.mycompany.simuladorjuegorol;
-
+import Modelos.*;
+import Vista.VistaCombate;
+import Controlador.ControladorCombate;
 /**
  *
  * @author usuario
@@ -11,6 +13,19 @@ package com.mycompany.simuladorjuegorol;
 public class SimuladorJuegoRol {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+        System.out.println("Simulador de DND");
+        //inicializar
+        Personaje explorador = new Ranger("david",14);
+        Personaje guerrero = new Paladin("valerie",17);
+        Personaje profesor = new Personaje("edwin",10);
+        
+        Personaje[] miGrupo ={explorador,guerrero,profesor};
+        VistaCombate miVista =new VistaCombate();
+        
+        //inicializamos el controlador (inyectar el modelo y la vista)
+        ControladorCombate controlador = new ControladorCombate(miGrupo,miVista);
+        
+        //la logica de la coordinacion
+        controlador.ejecutarRonda();
+   }
+ }
