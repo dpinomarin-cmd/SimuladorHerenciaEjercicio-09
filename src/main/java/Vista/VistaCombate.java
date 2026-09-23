01e274d2a0a9d47855a89057cda16329f70a12a5
usuario <usuario@DESKTOP-QOV8DKP>
2026-09-23 10:39:12 -0500

    juego de rol

---
+++ src/main/java/Vista/VistaCombate.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vista;
+
+/**
+ *
+ * @author usuario
+ */
+public class VistaCombate {
+   public void mostrarInicioDeCombate(){
+        System.out.println("=====================================");
+        System.out.println("================Inicio de combate================");
+        System.out.println("=====================================");
+    }
+    public void mostrarAtaque(String nombrePersonaje, String accionAtaque){
+        System.out.println("☄"+nombrePersonaje+"->"+ accionAtaque);
+    } 
+}
